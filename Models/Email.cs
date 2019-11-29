e3d62b0af3ffb5a9c68797a603e9c23e9ff3f879
Nastassia Karatkova <nastassia_karatkova@epam.com>
2019-11-29 09:51:51 +0300

    Add Models Layer

---
+++ Models/Email.cs
@@ -0,0 +1,11 @@
+﻿namespace Models
+{
+    public class Email
+    {
+        public string Recipient { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Text { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Models
+{
+    public class User
+    {
+        [JsonProperty("Login")]
+        public string Login { get; set; }
+
+        [JsonProperty("Password")]
+        public string Password { get; set; }
+    }
+}
