bf7da0600ea592df849264891fc50916b2e2a514
Nastassia Karatkova <nastassia_karatkova@epam.com>
2019-11-29 09:38:05 +0300

    initial commit

---
+++ EmailTests.cs
@@ -0,0 +1,58 @@
+﻿using EmailTests.TestData;
+using Models;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using PageObjects;
+
+namespace EmailTests
+{
+    [TestFixture]
+    public class EmailTests
+    {
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+        }
+
+        [Test]
+        public void SendEmail()
+        {
+            LogIn();
+            //Assert.That(_driver.Url.Contains("/inbox/"));
+
+            var emailForSending = new Email() { Recipient = Users.User.Login, Subject = "for fun", Text = "hey,there!" };
+
+            var email = CreateEmail(emailForSending);
+
+            email.SaveDraft();
+
+
+        }
+
+        private void LogIn()
+        {
+            new MailRuPage(_driver).Open().LogIn(Users.User);
+        }
+
+        private NewEmail CreateEmail(Email email)
+        {
+            return new NewEmail(_driver).Create(email);
+        }
+
+        private void GetLastDraft()
+        {
+
+        }
+    }
+}
---
+++ TestData/Users.cs
@@ -0,0 +1,29 @@
+﻿using Models;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace EmailTests.TestData
+{
+    public class Users
+    {
+        static Users()
+        {
+            User = JsonConvert.DeserializeObject<User>(GetJSONString());
+        }
+
+        //[JsonProperty("User")]
+        public static User User { get; set; }
+
+        public static string GetJSONString()
+        {
+            return File.ReadAllText($@"{GetExecutingDirectory()}\TestData\Users.json");
+        }
+        private static string GetExecutingDirectory()
+        {
+            var location = new Uri(Assembly.GetExecutingAssembly().GetName().CodeBase);
+            return new FileInfo(location.LocalPath).Directory.FullName;
+        }
+    }
+}
